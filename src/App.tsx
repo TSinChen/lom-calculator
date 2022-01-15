60dad541cbaa01ef251720e96fce378fcbc13059
TSin Chen <sinin60076@gmail.com>
2022-01-15 19:40:03 +0800

    github icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import { useState } from 'react'
+import { Fragment, useState } from 'react'
 
 import Form from './components/Form/Form'
+import GITHUB_ICON from './assets/icons/github.png'
 
 const App = () => {
   const [result, setResult] = useState('填完入伍日期還有折抵天數後就按下計算吧！')
 
   return (
-    <main className="container">
-      <Form setResult={setResult} />
-      <div className="result">{result}</div>
-    </main>
+    <Fragment>
+      <main className="container">
+        <h1 className="title">兵役計算機</h1>
+        <Form setResult={setResult} />
+        <div className="result">{result}</div>
+      </main>
+      <a
+        className="github"
+        href="https://github.com/TSinChen/lom-calculator"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <img src={GITHUB_ICON} />
+      </a>
+    </Fragment>
   )
 }
 
